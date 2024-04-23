84df2fb544e3dd1b81c22ceb6d0db147e4391f81
Ponsiano De Loor <pdeloor@sudamericano.fin.ec>
2024-04-22 21:59:02 -0500

    uso de Model para crear atributos en el controlador

--- src/main/java/com/apptics/thymeleaf/controller/MainController.java
+++ src/main/java/com/apptics/thymeleaf/controller/MainController.java
@@ -1,13 +1,16 @@
 package com.apptics.thymeleaf.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("name", "Ponsiano De Loor!");
+        model.addAttribute("message", "Hello, World!");
         return "index";
     }
 
